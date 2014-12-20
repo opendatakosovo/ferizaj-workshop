939be702f107cb760d12b54129a16f64fc45778c
Your Name <partin.imeri@gmail.com>
2014-12-20 21:51:47 +0100

    Added JSON Response

--- run.py
+++ run.py
@@ -1,22 +1,52 @@
-import csv
 from pymongo import MongoClient
+from flask import Flask, render_template, Response
+from bson import json_util
+
+app = Flask(__name__)
 
 mongo = MongoClient()
-db = mongo.ferizaji
-collection = db.tabela
+db = mongo.kosovoprocurements
+collection = db.procurements
 
+@app.route('/<string:komuna>/monthly-summary/<int:viti>')
+def hello(komuna,viti):
+    rezultati = collection.aggregate([{
+	        "$match":{
+	            "city":komuna,
+	            "viti":viti
+	            }
+	        },
+	    {
+	        "$group":{
+	           "_id": {
+	                "muaji":{
+	                    "$month":"$dataNenshkrimit"
+	                }
+	            },
+	            "vleraKontrates":{
+	                "$sum":"$kontrata.vlera"
+	            },
+	            "qmimi":{
+	                "$sum":"$kontrata.qmimi"
+	            }
+	        }
+	    },
+	    {
+	        "$project":{
+	            "_id":0,
+	            "muaji" :"$_id.muaji", 
+	            "vlera":"$vleraKontrates", 
+	            "qmimi":"$qmimi"
+			}
+	    },
+	    {
+	        "$sort":{
+	            "muaji":1
+	        }
+	    }
+	])
 
-def lexo_file():
-    with open("data/test.csv", "r") as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            emri = row[0]
-            mbiemri = row[1]
-            qyteti = row[2]
-            rezultati = {"emri": emri,
-                "mbiemri": mbiemri,
-                "qyteti": qyteti
-                }
-            print rezultati
-            collection.insert(rezultati)
-lexo_file()
+    return Response(response=json_util.dumps(rezultati), mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run(debug=True)
