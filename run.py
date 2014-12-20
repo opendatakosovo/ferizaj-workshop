9f1b4f9c81c835ebc8f277f7a45ec20caa6c6522
Vullkan92 <vullkan.halili@riinvest.net>
2014-12-20 23:07:33 +0100

    Changed port number and host

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
 from flask import Flask, render_template, Response
 from bson import json_util
+import argparse
 
 app = Flask(__name__)
 
 mongo = MongoClient()
 db = mongo.kosovoprocurements
 collection = db.procurements
+
+@app.route('/')
+def home():
+	return "<h1>0.0.0.0:5030/string:komuna/monthly-summary/int:viti</h1>"
 
 @app.route('/<string:komuna>/monthly-summary/<int:viti>')
 def hello(komuna,viti):
@@ -49,4 +54,14 @@ def hello(komuna,viti):
     return Response(response=json_util.dumps(rezultati), mimetype="application/json")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    # Define the arguments.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='0.0.0.0', help='Host to bind to: [%(default)s].')
+    parser.add_argument('--port', type=int, default=5030, help='Port to listen to: [%(default)s].')
+    parser.add_argument('--debug', action='store_true', default=True, help='Debug mode: [%(default)s].')
+
+    # Parse arguemnts and run the app.
+    args = parser.parse_args()
+    app.run(debug=args.debug, host=args.host, port=args.port)
+
