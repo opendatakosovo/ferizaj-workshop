1cfea2eee0a47ae1022d7799a7523eca26906e07
Your Name <partin.imeri@gmail.com>
2014-12-21 11:29:44 +0100

    Added new responses to the API

--- run.py
+++ run.py
@@ -63,7 +63,40 @@ def hello(komuna, viti):
 def tipi_prokurimit():
     #TODO: Ekzekuto Query dhe merr te dhenat per tipet e prokurimit
     # Per Ferizaj, viti 2011
-    return "Kthe pergjigjen si JSON"
+    rezultati = collection.aggregate([
+        {
+            "$match": {
+                "city": "ferizaj",
+                "viti": 2011
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "tipi": "$tipi"
+                },
+                "shuma": {
+                    "$sum": "$kontrata.vlera"}
+            }
+        },
+        {
+            "$sort": {
+                "_id.tipi": 1
+            }
+        },
+        {
+            "$project": {
+                "tipi": "$_id.tipi",
+                "shuma": "$shuma",
+                "_id": 0
+            }
+        }
+    ])
+
+    resp = Response(
+        response=json_util.dumps(rezultati['result']),
+        mimetype='application/json')
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5030)
